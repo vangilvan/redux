73b6811e3d8379068a5987315aa1cc9b608fb869
vangilvan <vangilvan2@gmail.com>
2021-02-20 16:35:49 -0300

    Redux correto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {useSelector, useDispatch} from 'react-redux'
+
+
 
 function App() {
+  const [inputFrutas, setInputFrutas] = React.useState("")
+  const [inputTitulo, setInputTitulo] = React.useState("")
+
+  const frutas = useSelector((state) => state.frutasReducer.frutas)
+
+  const titulo = useSelector((state) => state.tituloReducer.titulo)
+  console.log(titulo)
+
+  const dispatch = useDispatch()
+
+  function adicionarFruta(event){
+    event.preventDefault()
+
+    const objFruta = {
+      nome: inputFrutas
+    }
+
+    dispatch({type: "ADICIONAR", value: objFruta})
+  }
+
+  function alterarTitulo(event){
+    setInputTitulo(event.target.value)
+    dispatch({type: "ALTERAR", value: event.target.value})
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="row m-2">
+      
+      <h1 className="primary display-4">{titulo}</h1>
+      
+      <form>
+      <div className="input-group mb-3">
+        <label className="input-group-text">Título:</label>
+        <input placeholder="Digite o título" className="form-control" value={inputTitulo} onChange={alterarTitulo}/>
+      </div>
+      </form>
+      
+      
+      <form onSubmit={adicionarFruta}>
+      <div className="input-group mb-3">
+        <input placeholder="Criar lista de frutas" className="form-control" value={inputFrutas} onChange={(event) => setInputFrutas(event.target.value)}/>
+        <button className="input-group-text">Enviar</button>
+        </div>
+      </form>
+      {frutas.map( (fruta, index) => {
+        return(
+          <li key={index}>{fruta.nome}</li>
+        )
+      })}
     </div>
   );
 }
---
+++ src/reducers/index.js
@@ -0,0 +1,32 @@
+import { combineReducers } from 'redux'
+
+
+
+
+const initialState = {
+    frutas: [ ]
+}
+
+function frutasReducer(state = initialState, action){
+    if(action.type === "ADICIONAR"){
+        return {frutas: [
+            ...state.frutas, {...action.value}]}
+    }
+    else {
+        return state
+    }
+}
+
+
+function tituloReducer(state = {titulo: "Título"}, action){
+    if(action.type === "ALTERAR"){
+        return {titulo: action.value}
+    }
+    else {
+        return state
+    }
+}
+
+const reducers = combineReducers({frutasReducer, tituloReducer})
+
+export default reducers
